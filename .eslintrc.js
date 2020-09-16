4a5aa7f64fe41d4177fb4921f409d6e60208b5d7
Artemy <48246993+quertc@users.noreply.github.com>
2020-09-17 00:29:23 +0300

    Updated dependencies and installed ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+    'arrow-parens': ['error', 'as-needed'],
+  },
+};
